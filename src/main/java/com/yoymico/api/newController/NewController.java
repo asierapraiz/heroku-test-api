9c48ffedc788b7293da5d183b10d70226be729f4
asier <asierapraiz@gmail.com>
2021-03-09 17:33:28 +0100

    fist commit

---
+++ src/main/java/com/yoymico/api/newController/NewController.java
@@ -0,0 +1,15 @@
+package com.yoymico.api.newController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apo")
+public class NewController {
+
+    @GetMapping("/homo")
+    public String home(){
+    return "Hola mundo nuevo";}
+}
